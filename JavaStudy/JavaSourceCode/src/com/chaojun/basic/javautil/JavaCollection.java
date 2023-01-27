0c224af446e7ba2e63727bba705c6509e14f4b7e
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-01-27 13:32:53 +0800

    Coding the Java Collection

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaCollection.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaCollection.java
@@ -2,7 +2,9 @@ package com.chaojun.basic.javautil;
 
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaCollection {
     public static void main(String[] args) {
@@ -27,7 +29,17 @@ public class JavaCollection {
         list.removeIf(str -> str.contains("1"));
         System.out.println(list);
 
+        //使用lambda表达式传递条件给方法，  使用到了Predicate对象
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("123");
+        list2.add("145");
+        list2.add("167");
+        list2.add("8920");
+        int i = judgeResult(list2, str -> str.contains("1"));
+        System.out.println("i=" + i);
 
+
+        //Stream的使用,
         IntStream is = IntStream.builder()
                 .add(6)
                 .add(5)
@@ -35,7 +47,43 @@ public class JavaCollection {
                 .add(4)
                 .add(8)
                 .build();
-        OptionalInt max = is.max();
-        System.out.println(max.getAsInt());
+        //获取最大值
+//        OptionalInt max = is.max();
+//        System.out.println("max="+max);
+        //获取最小值
+//        OptionalInt min = is.min();
+//        System.out.println("min="+min);
+        //获取总值
+//        System.out.println(is.sum());
+        //获取元素个数
+//        System.out.println(is.count());
+        //获取平均值
+//        System.out.println(is.average());
+        //判断是否全部满足lambda表达式的条件
+//        boolean result = is.allMatch(value -> value > 4);
+//        System.out.println("result="+result);
+        //判断是否有满足lambda表达式的条件
+//        boolean result = is.anyMatch(value -> value > 7);
+//        System.out.println("result="+result);
+        //改变每个值后返回新的IntStream
+//        IntStream intStream = is.map(value -> value * 2);
+//        intStream.forEach(value -> System.out.println(value));
+        //使用中间方法对Instream进行排序
+//        IntStream sorted = intStream.sorted();
+//        sorted.forEach(str-> System.out.println(str));
+
+        //使用Collection转Stream方法操作集合
+        Stream<String> stringStream = list2.stream().filter(value -> value.length() > 3);
+        System.out.println(stringStream.findFirst());
+    }
+
+    public static int judgeResult(Collection<String> collection, Predicate<String> predicate) {
+        int total = 0;
+        for (String str : collection) {
+            if (predicate.test(str)) {
+                total++;
+            }
+        }
+        return total;
     }
 }
