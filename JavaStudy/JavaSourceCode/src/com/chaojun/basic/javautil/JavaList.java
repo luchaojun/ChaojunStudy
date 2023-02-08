349dc0d6cd6d392ee9fb1104fe11fd2d5e9617c8
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-02-08 17:17:17 +0800

    Tidy up the JavaList.java

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaList.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaList.java
@@ -1,9 +1,6 @@
 package com.chaojun.basic.javautil;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Objects;
+import java.util.*;
 
 public class JavaList {
     public static void main(String[] args) {
@@ -43,60 +40,3 @@ public class JavaList {
     }
 }
 
-class Person{
-    private int id;
-    private String name;
-    private int age;
-
-    public Person(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return id == person.id && age == person.age && Objects.equals(name, person.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
