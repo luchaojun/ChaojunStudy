293ddaa5742c47fbf8f7f478fcd10d9630620fd8
Chaojun_Lu <773662313@qq.com>
2023-02-09 23:21:26 +0800

    Add map key object change, get and remove by key fail

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaMap.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/javautil/JavaMap.java
@@ -1,16 +1,83 @@
 package com.chaojun.basic.javautil;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Objects;
 
 public class JavaMap {
     public static void main(String[] args) {
         //测试HashMap加入数据比对equals方法和hashcode方法
-        Person p1 = new Person(1, "cj1", 22);
-        Person p2 = new Person(1, "cj1", 22);
-        HashMap<Person, String> map = new HashMap<>();
-        map.put(p1, "1");
-        map.put(p2, "1");
-        System.out.println(map);
-        System.out.println(map.containsKey(new Person(2, "cj1", 22)));
-        System.out.println(map.containsValue(new Person(2, "cj1", 22)));
+//        Person p1 = new Person(1, "cj1", 22);
+//        Person p2 = new Person(1, "cj1", 22);
+//        HashMap<Person, String> map = new HashMap<>();
+//        map.put(p1, "1");
+//        map.put(p2, "1");
+//        System.out.println(map);
+//        System.out.println(map.containsKey(new Person(2, "cj1", 22)));
+//        System.out.println(map.containsValue(new Person(2, "cj1", 22)));
+
+//        Person p1 = new Person(1, "cj1", 22);
+//        Person p2 = new Person(2, "cj2", 23);
+//        Person p3 = new Person(3, "cj3", 24);
+//        Person p4 = new Person(4, "cj4", 25);
+//        Person p5 = new Person(5, "cj5", 26);
+//        HashMap<Person, String> map = new HashMap<>();
+//        map.put(p1, "1");
+//        map.put(p2, "2");
+//        map.put(p3, "3");
+//        map.put(p4, "4");
+//        map.put(p5, "5");
+//        //放入map集合当中的key是对象, 修改此key对象后不能被查询到也不能不删除
+//        p3.setName("cjj3");
+////        System.out.println(map.get(p3));
+////        map.remove(p3);
+////        System.out.println(map);
+//        System.out.println(map);
+
+//        HashMap<A, String> map = new HashMap<>();
+//        map.put(new A(10), "cj1");
+//        map.put(new A(20), "cj2");
+//        Iterator<A> iterator = map.keySet().iterator();
+//        A a = iterator.next();
+//        System.out.println("a.count="+a.getCount());
+//        a.setCount(10);
+//        System.out.println(map.remove(new A(10)));
+//        System.out.println(map.get(new A(20)));
+//        System.out.println(map.get(new A(10)));
+    }
+}
+
+class A{
+    private int count;
+
+    public A(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "count=" + count +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        A a = (A) o;
+        return count == a.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.count;
     }
 }
