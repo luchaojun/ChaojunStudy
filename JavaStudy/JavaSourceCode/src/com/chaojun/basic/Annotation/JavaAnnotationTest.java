0331c0eee868fde4725b11790019348ab18f9d5a
Chaojun_Lu <773662313@qq.com>
2023-03-01 22:57:56 +0800

    Add the annotation study into java file

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest.java
@@ -1,10 +1,25 @@
 package com.chaojun.basic.Annotation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//SuppressWarning注解的使用
+@SuppressWarnings(value = "unchecked")
 public class JavaAnnotationTest {
     public static void main(String[] args) {
         JavaAnnotationTest.test();
+
+        List list = new ArrayList<Integer>();
+        list.add(1);
+        List<String> l = list;
+        System.out.println(l.get(0));
     }
 
+    public void testAnnotation(){
+        ArrayList<String> list = new ArrayList();
+    }
+
+    //Deprecated注解的使用
     @Deprecated(since = "3", forRemoval = true)
     public static void test(){
 
