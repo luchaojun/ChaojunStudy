503578cddd8e7753eb3b7b13965e53c204219c03
Chaojun_Lu <773662313@qq.com>
2023-03-10 00:20:23 +0800

    Add annotation study into java

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest2.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest2.java
@@ -1,22 +1,33 @@
 package com.chaojun.basic.Annotation;
 
 import java.lang.annotation.*;
+import java.lang.reflect.Constructor;
 
 public class JavaAnnotationTest2 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NoSuchMethodException {
+        Annotation[] annotations = TestAnno.class.getMethod("test").getAnnotations();
+        for (Annotation a: annotations) {
+            if(a instanceof MyAnno){
+                int age = ((MyAnno) a).age();
+                String name = ((MyAnno) a).name();
+                System.out.println("age="+age+",name="+name);
+            }
+        }
     }
 
     @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.TYPE)
+    @Target(ElementType.METHOD)
     @Inherited
     @interface MyAnno{
         int age() default 12;
         String name() default "qc";
     }
 
-    @MyAnno(name="cj", age=11)
-    class TestAnno{
 
+    class TestAnno{
+        @MyAnno(name="cj", age=11)
+        public void test(){
+
+        }
     }
 }
