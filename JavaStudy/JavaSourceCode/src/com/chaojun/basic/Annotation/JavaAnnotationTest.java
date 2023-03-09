7c9d123f2a47fb0f0452289ac6fd9f688931664e
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-03-09 09:05:19 +0800

    Add Annotation Study into java

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest.java
@@ -4,19 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 //SuppressWarning注解的使用
-@SuppressWarnings(value = "unchecked")
+//@SuppressWarnings(value = "unchecked")
 public class JavaAnnotationTest {
     public static void main(String[] args) {
         JavaAnnotationTest.test();
 
-        List list = new ArrayList<Integer>();
-        list.add(1);
-        List<String> l = list;
-        System.out.println(l.get(0));
+        //堆污染
+//        List list = new ArrayList<Integer>();
+//        list.add(1);
+//        List<String> l = list;
+//        System.out.println(l.get(0));
+
+        //测试可能有堆污染的方法 使用@SafeVarargs， 消除堆污染
+        JavaAnnotationTest.testAnnotation("123", "456");
     }
 
-    public void testAnnotation(){
-        ArrayList<String> list = new ArrayList();
+    //测试堆污染
+    @SafeVarargs
+    public static <T> void testAnnotation(T ... strs){
+
     }
 
     //Deprecated注解的使用
