6216717acdb1dbde11466dfbaf70e16ffe29fc47
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-03-11 18:45:26 +0800

    Add the annotation study into javaa file

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest3.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest3.java
@@ -6,28 +6,76 @@ import java.lang.annotation.*;
 //自定义重复注解
 public class JavaAnnotationTest3 {
     public static void main(String[] args) {
+        //获取指定注解 可以获取extends注解
+//        Class myTag = MyTag.class;
+//        Annotation annotation = myTag.getAnnotation(MyTag.class);
+//        System.out.println(annotation);
 
+        //获取全部注解 包括extends的注解
+//        Class myTag = MyTag.class;
+//        Annotation[] annotations = myTag.getAnnotations();
+//        for (Annotation a : annotations) {
+//            System.out.println(a);
+//        }
+
+        //获取指定注解 不能获取extends的注解
+//        Class myTag = TestExtendsTag2.class;
+//        Annotation annotation = myTag.getDeclaredAnnotation(ExtendsTag.class);
+//        System.out.println(annotation);
+
+
+        //获取全部注解 不能获取extends
+//        Class myTag = MyTag.class;
+//        Annotation[] annotations = myTag.getDeclaredAnnotations();
+//        for (Annotation a : annotations) {
+//            System.out.println(a);
+//        }
+
+        Class myTag = TestExtendsTag2.class;
+        System.out.println(myTag.isAnnotationPresent(ExtendsTag.class));
     }
 }
 
+
+//Repeatable注解的使用可以进行重复注解
 @Repeatable(MyTags.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@interface MyTag{
+@interface MyTag {
     int number();
+
     String name();
 }
 
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@interface MyTags{
+@interface MyTags {
     MyTag[] value();
 }
 
 
-@MyTag(number=123, name="cj")
-@MyTag(number=456, name="qc")
-class TestRepeatTag{
+@MyTag(number = 123, name = "cj")
+@MyTag(number = 456, name = "qc")
+class TestRepeatTag {
 
 }
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+@interface ExtendsTag{
+
+}
+
+@ExtendsTag
+class TestExtendsTag{
+
+}
+
+@Deprecated
+class TestExtendsTag2 extends TestExtendsTag{
+
+}
+
