3d23b6d40f2c8dbecceaccd66e843086fb4e9e1e
Chaojun_Lu <773662313@qq.com>
2023-03-12 23:02:56 +0800

    Add the java annotation into java file

--- JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest3.java
+++ JavaStudy/JavaSourceCode/src/com/chaojun/basic/Annotation/JavaAnnotationTest3.java
@@ -7,12 +7,12 @@ import java.lang.annotation.*;
 public class JavaAnnotationTest3 {
     public static void main(String[] args) {
         //获取指定注解 可以获取extends注解
-//        Class myTag = MyTag.class;
+//        Class myTag = TestRepeatTag.class;
 //        Annotation annotation = myTag.getAnnotation(MyTag.class);
 //        System.out.println(annotation);
 
         //获取全部注解 包括extends的注解
-//        Class myTag = MyTag.class;
+//        Class myTag = TestRepeatTag.class;
 //        Annotation[] annotations = myTag.getAnnotations();
 //        for (Annotation a : annotations) {
 //            System.out.println(a);
@@ -25,14 +25,29 @@ public class JavaAnnotationTest3 {
 
 
         //获取全部注解 不能获取extends
-//        Class myTag = MyTag.class;
+//        Class myTag = TestRepeatTag.class;
 //        Annotation[] annotations = myTag.getDeclaredAnnotations();
 //        for (Annotation a : annotations) {
 //            System.out.println(a);
 //        }
 
-        Class myTag = TestExtendsTag2.class;
-        System.out.println(myTag.isAnnotationPresent(ExtendsTag.class));
+        //判断是否含有ExtendsTag注解
+//        Class myTag = TestExtendsTag2.class;
+//        System.out.println(myTag.isAnnotationPresent(ExtendsTag.class));
+
+        //获取重复注解
+//        Class<TestRepeatTag> clazz = TestRepeatTag.class;
+//        MyTag[] annotations = clazz.getAnnotationsByType(MyTag.class);
+//        for (MyTag tag: annotations) {
+//            System.out.println(tag);
+//        }
+
+        //获取重复注解
+//        Class<TestRepeatTag> clazz = TestRepeatTag.class;
+//        MyTag[] annotations = clazz.getDeclaredAnnotationsByType(MyTag.class);
+//        for (MyTag tag: annotations) {
+//            System.out.println(tag);
+//        }
     }
 }
 
@@ -55,6 +70,7 @@ public class JavaAnnotationTest3 {
 }
 
 
+@Deprecated
 @MyTag(number = 123, name = "cj")
 @MyTag(number = 456, name = "qc")
 class TestRepeatTag {
