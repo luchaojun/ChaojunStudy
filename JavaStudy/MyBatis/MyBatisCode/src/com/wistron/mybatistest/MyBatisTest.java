78abba9e909131d1647b7893345bf642d6a8f515
Chaojun_Lu <773662313@qq.com>
2023-03-22 00:16:05 +0800

    Add MyBatis Study into java file

---
+++ JavaStudy/MyBatis/MyBatisCode/src/com/wistron/mybatistest/MyBatisTest.java
@@ -0,0 +1,12 @@
+package com.wistron.mybatistest;
+
+/**
+ * @Author: Chaojun_Lu
+ * @Date: 2023/3/21
+ * @Function:
+ */
+public class MyBatisTest {
+    public static void main(String[] args) {
+
+    }
+}
